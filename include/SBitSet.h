09813c8022dd34083dcdd3fa401fc7c9aa913294
H Wang <haoyuanwang102@gmail.com>
2022-11-13 23:28:38 -0800

    Update data structure

--- include/SBitSet.h
+++ include/SBitSet.h
@@ -6,9 +6,7 @@
 #ifndef RCP_SBITSET_H
 #define RCP_SBITSET_H
 
-#include <bit>
-#include <vector>
-#include <stdint.h>
+#include "rcp_common.h"
 
 #define MAX_ELEMS UINT32_MAX
 
@@ -16,73 +14,22 @@ class SBitSet {
 private:
     std::vector<uint64_t> dat;
     uint32_t max_elem = 0;
+    uint32_t num_elem = 0;
 
 public:
-    SBitSet() {};
+    SBitSet() = default;
+    // Build bitset from multiple source
+    explicit SBitSet(const std::vector<const SBitSet*>& srcs);
 
-    bool contains(uint32_t elem) {
-        if (elem > max_elem) {
-            return false;
-        }
-        uint32_t dat_pos = elem / 64;
-        uint32_t bit_pos = elem % 64;
-        uint64_t mask = 0x1l << bit_pos;
+    bool contains(uint32_t elem);
 
-        return (dat[dat_pos] & mask) != 0;
-    }
+    void insert(uint32_t elem);
 
-    void insert(uint32_t elem) {
-        uint32_t dat_pos_new = elem / 64;
-        uint32_t bit_pos_new = elem % 64;
+    uint32_t size();
+    uint32_t calculate_size();
 
-        uint32_t dat_pos_old = this -> max_elem / 64;
+    std::vector<uint32_t>* get_elems();
 
-        if (dat_pos_new >= dat.size()) {
-            uint32_t space_to_grow = dat_pos_new + 1 - dat.size();
-            this -> dat.insert(this -> dat.end(), space_to_grow, 0);
-        }
-
-        uint64_t old_data = this -> dat[dat_pos_new];
-        uint64_t new_data = old_data | (0x1l << bit_pos_new);
-        this -> dat[dat_pos_new] = new_data;
-
-        this -> max_elem = std::max(elem, this -> max_elem);
-    }
-
-    uint32_t size() {
-        uint32_t num_elms = 0;
-
-        for (auto& blk : this -> dat) {
-            num_elms += std::popcount(blk);
-        }
-
-        return num_elms;
-    }
-
-    std::vector<uint32_t>* get_elems() {
-        // Note: Users are responsible for free the memory!!
-        auto ret = new std::vector<uint32_t>;
-
-        uint32_t cursor = 0;
-
-        while (cursor < max_elem) {
-            uint32_t dat_pos = cursor / 64;
-            uint32_t bit_pos = cursor % 64;
-
-            if (this -> dat[dat_pos] == 0) {
-                cursor += 64;
-            } else {
-                uint64_t mask = 0x1l << bit_pos;
-                if ((this -> dat[dat_pos] & mask) != 0) {
-                    // Hit
-                    ret->push_back(cursor);
-                }
-                cursor ++;
-            }
-        }
-
-        return ret;
-    }
 };
 
 #endif //RCP_SBITSET_H
---
+++ src/SBitSet.cpp
@@ -0,0 +1,100 @@
+//
+// Created by Haoyuan Wang on 11/13/22.
+//
+
+#include "SBitSet.h"
+#include <bit>
+
+SBitSet::SBitSet(const std::vector<const SBitSet*>& srcs) {
+    uint32_t max_blks = 0;
+    for (auto src: srcs) {
+        this -> max_elem = std::max(this -> max_elem, src -> max_elem);
+        max_blks = std::max(max_blks, static_cast<uint32_t>(src -> dat.size()));
+    }
+
+    // grow data
+    this -> dat.assign(max_blks, 0);
+
+    // save data
+    for (uint32_t blk_id = 0; blk_id < max_blks; blk_id++) {
+        uint64_t blk = 0;
+        for (auto src: srcs) {
+            if (blk_id < src -> dat.size()) {
+                blk |= src -> dat[blk_id];
+            }
+        }
+        this -> dat[blk_id] = blk;
+    }
+
+    // update num_elem
+    this -> num_elem = this -> calculate_size();
+}
+
+bool SBitSet::contains(uint32_t elem) {
+    if (elem > max_elem) {
+        return false;
+    }
+    uint32_t dat_pos = elem / 64;
+    uint32_t bit_pos = elem % 64;
+    uint64_t mask = 0x1l << bit_pos;
+
+    return (dat[dat_pos] & mask) != 0;
+}
+
+void SBitSet::insert(uint32_t elem) {
+    uint32_t dat_pos_new = elem / 64;
+    uint32_t bit_pos_new = elem % 64;
+
+    uint32_t dat_pos_old = this -> max_elem / 64;
+
+    if (dat_pos_new >= dat.size()) {
+        uint32_t space_to_grow = dat_pos_new + 1 - dat.size();
+        this -> dat.insert(this -> dat.end(), space_to_grow, 0);
+    }
+
+    uint64_t old_data = this -> dat[dat_pos_new];
+    uint64_t new_data = old_data | (0x1l << bit_pos_new);
+    this -> dat[dat_pos_new] = new_data;
+
+    this -> max_elem = std::max(elem, this -> max_elem);
+    this -> num_elem ++;
+}
+
+uint32_t SBitSet::size() {
+    return this -> num_elem;
+}
+
+uint32_t SBitSet::calculate_size() {
+    uint32_t num_elms = 0;
+
+    for (auto& blk : this -> dat) {
+        num_elms += std::popcount(blk);
+    }
+
+    return num_elms;
+}
+
+std::vector<uint32_t>* SBitSet::get_elems() {
+    // Note: Users are responsible for free the memory!!
+    auto ret = new std::vector<uint32_t>;
+
+    uint32_t cursor = 0;
+
+    while (cursor < max_elem) {
+        uint32_t dat_pos = cursor / 64;
+        uint32_t bit_pos = cursor % 64;
+
+        if (this -> dat[dat_pos] == 0) {
+            cursor += 64;
+        } else {
+            uint64_t mask = 0x1l << bit_pos;
+            if ((this -> dat[dat_pos] & mask) != 0) {
+                // Hit
+                ret->push_back(cursor);
+            }
+            cursor ++;
+        }
+    }
+
+    return ret;
+}
